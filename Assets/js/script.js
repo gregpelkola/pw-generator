7b372507761d03e6205d213f35d637e03e75bae4
Greg Pelkola <141973953+gregpelkola@users.noreply.github.com>
2023-09-23 14:02:36 -0500

    linked js to html created prompts

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -12,12 +12,19 @@ function writePassword() {
 };
 
 function generatePassword() {
-  var newPass = "test";
- 
-
-
+  var newPass = ""
+  var lowerLet = "abcdefghijklmnopqrstuvwxyz";
+  var upperLet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numbers = "0123456789";
+  var specChar = ")(*&^%$#@!";
+  var password = prompt("Does the password contain lowercase letters?");
+  var password = prompt("Does the password contain uppercase letters?");
+  var password = prompt("Does the password contain numbers?");
+  var password = prompt("Does the password contain special charcters?");
+  var charLength = "<=8, >=128";
 
   return newPass;
+  
 };
 
 // Add event listener to generate button
